06e4f72ef1dad5f6fd3f1b51c5a095bacf3ac160
chaimaa elakfa <chaimaaelakfa753@gmail.com>
2023-05-12 17:22:59 +0100

    initial commit

---
+++ app/src/main/java/com/example/test_your_brain/start_screen.kt
@@ -0,0 +1,11 @@
+package com.example.test_your_brain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class start_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_screen)
+    }
+}
